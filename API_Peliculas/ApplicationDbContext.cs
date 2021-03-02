c36c8e4a4c429d88d8315b389a4d3ccf6e973f4b
Maycol Lain <maycollain11@gmail.com>
2021-03-01 22:25:40 -0300

    Creacion de la entidad Genero

---
+++ API_Peliculas/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using API_Peliculas.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Peliculas
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) :base(options)
+        {
+        }
+
+        public DbSet<Genero> Generos { get; set; }
+    }
+}
